35a1d102953d026fc7082c71769b168723b2ece7
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-03-24 22:48:34 +0400

    rackcloud work

--- mycloudrack/mycloudrack/settings.py
+++ mycloudrack/mycloudrack/settings.py
@@ -9,10 +9,9 @@ USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
 
 ITEM_PIPELINES = [
         'mycloudrack.tmp_pipelines.UrlPipeline',
-        # 'mycloudrack.pipelines.MongoDBPipeline',
-        # 'basecrawler.pipelines.StatusPipeline'
+        'mycloudrack.pipelines.MongoDBPipeline',
+        'basecrawler.pipelines.StatusPipeline'
         ]
-EXTENSIONS = {}
 
 BASE_URL = "https://mycloud.rackspace.com"
 URLS = dict(
@@ -26,7 +25,7 @@ URLS = dict(
     _URL_DNS = "/proxy/rax:dns,cloudDNS/domains/",
     _URL_FILES = "/proxy/object-store,cloudFiles,{region}/",
     _URL_DATABASE = "/proxy/rax:database,cloudDatabases,{region}/instances",
-    _URL_BACKUP = "/services/v1.0/user/agents/"
+    _URL_BACKUP = "https://clouddrive.rackspace.com/services/v1.0/user/agents/"
         )
 
 REGIONS = (

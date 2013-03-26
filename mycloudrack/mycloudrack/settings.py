7979d1b7f428789e77912ca6724e98c1c0eb06ff
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-03-26 05:40:41 +0400

    fix mycloud limits

--- mycloudrack/mycloudrack/settings.py
+++ mycloudrack/mycloudrack/settings.py
@@ -17,13 +17,13 @@ BASE_URL = "https://mycloud.rackspace.com"
 URLS = dict(
 
     _URL_LOGIN = "/",
-    _URL_INVOICES = "/proxy/rax:billing,billing/billing-summary/",
+    _URL_INVOICES = "/proxy/rax:billing,billing/billing-summary/?limit=100",
     _URL_INVOICE = "/proxy/rax:billing,billing/invoices/",
     _URL_SERVERS_ADD = "/proxy/compute,cloudServersOpenStack,{region}/servers/detail",
     _URL_SERVERS = "/proxy/compute,cloudServers/servers/detail",
     _URL_BALANCER = "/proxy/rax:load-balancer,cloudLoadBalancers,{region}/loadbalancers",
-    _URL_DNS = "/proxy/rax:dns,cloudDNS/domains/",
-    _URL_FILES = "/proxy/object-store,cloudFiles,{region}/",
+    _URL_DNS = "/proxy/rax:dns,cloudDNS/domains/?offset=0&limit=100",
+    _URL_FILES = "/proxy/object-store,cloudFiles,{region}/?limit=100",
     _URL_DATABASE = "/proxy/rax:database,cloudDatabases,{region}/instances",
     _URL_BACKUP = "https://clouddrive.rackspace.com/services/v1.0/user/agents/"
         )
